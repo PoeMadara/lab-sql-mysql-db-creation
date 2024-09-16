66f08fbc3ca8b5daba74d60863a31e65dbe9d662
Carlos Vergara <the.boss.patriot@gmail.com>
2024-09-16 22:31:41 +0200

    Solved Lab
    Database creation

---
+++ seeding.sql
@@ -0,0 +1,31 @@
+INSERT INTO CAR (VIN, manufacturer, model, year, color) 
+VALUES 
+('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
+('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
+('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
+('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
+('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
+('NEWVIN12345678901', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
+
+INSERT INTO CUSTOMER (name, phone, email, address, city, state_province, country, zip_postal_code) 
+VALUES 
+('Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+('Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+('Napoléon Bonaparte', '+33 1 79 75 40 00', '-', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+
+INSERT INTO SALESPERSON (name, store) 
+VALUES 
+('Petey Cruiser', 'Madrid'),
+('Anna Sthesia', 'Barcelona'),
+('Paul Molive', 'Berlin'),
+('Gail Forcewind', 'Paris'),
+('Paige Turner', 'Miami'),
+('Bob Frapples', 'Mexico City'),
+('Walter Melon', 'Amsterdam'),
+('Shonda Leer', 'São Paulo');
+
+INSERT INTO INVOICE (invoiceNumber, date, carVIN, customerID, staffID)
+VALUES 
+(852399038, '2018-08-22', '3K096I98581DHSNUP', 1, 3),
+(731166526, '2018-12-31', 'RKXVNNIHLVVZOUB4M', 3, 5),
+(271135104, '2019-01-22', 'ZM8G7BEUQZ97IH46V', 2, 7);
